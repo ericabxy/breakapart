fd8843d86a8d23d03a2fcc1d4976548548c3cfa9
Eric Abides <ericxdu23@gmail.com>
2026-06-23 07:03:46 -0700

    Missiles destroy asteroids.

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ local window = require('src.window')
 
 local asteroids = {}
 local bullets = {}
+local controllers = {}
 local explosions = {}
 local ships = {}
 
@@ -23,16 +24,25 @@ function love.update(dt)
     for y = #ships, 1, -1 do
       local o2 = ships[y]
       if o:is_circle_touching_circle(o2) then
+        controllers[o2.controller_number] = nil
         table.insert(explosions, explosion:new{ x = o2.x, y = o2.y })
         table.remove(ships, y)
       end
     end
   end
-  for x = #bullets, 1, -1 do
-    local o = bullets[x]
-    o:update(dt)
-    o:wrap(window)
-    if o.time_left <= 0 then table.remove(bullets, x) end
+  for bullet_i = #bullets, 1, -1 do
+    local this_bullet = bullets[bullet_i]
+    this_bullet:update(dt)
+    this_bullet:wrap(window)
+    if this_bullet.time_left <= 0 then table.remove(bullets, bullet_i) end
+    for asteroid_i = #asteroids, 1, -1 do
+      local this_asteroid = asteroids[asteroid_i]
+      if this_bullet:is_circle_touching_circle(this_asteroid) then
+        table.remove(bullets, bullet_i)
+        table.remove(asteroids, asteroid_i)
+        break
+      end
+    end
   end
   for x = #explosions, 1, -1 do
     local o = explosions[x]
@@ -56,7 +66,8 @@ end
 
 function love.joystickpressed(n, b)
   n, b = n + 1, b + 1
-  if b == RETRO_DEVICE_ID_JOYPAD_START and not ships[n] then
+  if b == RETRO_DEVICE_ID_JOYPAD_START and not controllers[n] then
     table.insert(ships, ship:new{ controller_number = n, x = window.width / 2, y = window.height / 2 })
+    controllers[n] = ships[#ships]
   end
 end
